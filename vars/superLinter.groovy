16ff3289332a2be3b3d51012d884c6faca3108c7
Roberto Polli <robipolli@gmail.com>
2022-12-15 16:18:18 +0100

    Basic super-linter pipeline

--- vars/superLinter.groovy
+++ vars/superLinter.groovy
@@ -16,10 +16,6 @@ def call(Map config = [:]) {
                 }
                 steps {
                     sh "env | sort"
-                    // echo "${agentLabel}"
-                    // echo "${pipelineParams.osConfiguration}"
-                    // echo "${pipelineParams.osConfiguration.OS_VERSION}"
-                    // echo "${pipelineParams.osConfiguration.DIR_TYPE}" 
                     sh """
                         pwd
                         /action/lib/linter.sh
