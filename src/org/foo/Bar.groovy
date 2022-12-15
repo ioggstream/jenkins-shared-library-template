14da4bc1844db30003ab6b3cac549f3be2f79e15
Roberto Polli <robipolli@gmail.com>
2022-12-15 15:11:47 +0100

    Simplify

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -5,8 +5,8 @@ import groovy.json.JsonSlurper
 class Bar {
 
     static String loadName(String key) {
-        def json = new JsonSlurper().parse(Bar.getResourceAsStream("bar.json"))
-        return json.names[key]
+        def json = Bar.getResourceAsStream("bar.json")
+        return json
     }
 
 }
